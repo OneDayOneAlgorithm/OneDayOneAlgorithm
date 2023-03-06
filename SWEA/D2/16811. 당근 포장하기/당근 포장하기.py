3566047146e1b03f8e9345ad66526111636e5b42
김형진 <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-03-06 10:44:39 +0900

    [D2] Title: 당근 포장하기, Time: 173 ms, Memory: 62,004 KB -BaekjoonHub

---
+++ SWEA/D2/16811. 당근 포장하기/당근 포장하기.py
@@ -0,0 +1,19 @@
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    lst = list(map(int,input().split()))
+    lst.sort()
+    minV = 1000
+    for i in range(N-2):
+        for j in range(i+1,N-1):
+            if lst[i] != lst[i+1] and lst[j] != lst[j+1]:
+                A = i+1
+                B = j-i
+                C =N-j-1
+                if A*B*C!=0 and A<=N//2 and B<=N//2 and C<=N//2:
+                    if minV > max(A,B,C) - min(A,B,C):
+                        minV = max(A,B,C) - min(A,B,C)
+
+    if minV == 1000:
+        minV = -1
+    print(f'#{tc} {minV}')
