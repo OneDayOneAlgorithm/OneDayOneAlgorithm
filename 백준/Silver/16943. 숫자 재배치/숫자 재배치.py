51217246b3c27679df7e0a10112f3ced5c6763ad
김형진 <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-03-11 22:56:20 +0900

    [Silver I] Title: 숫자 재배치, Time: 268 ms, Memory: 139324 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
@@ -1,14 +1,14 @@
 def solve(n):
-    if len(n) == len(A):
-        if int(n) < B and n[0] != '0':
-            lst.append(int(n))
+    if len(n) == len(A):    # 입력값의 자릿수가 A와 일치하고
+        if int(n) < B and n[0] != '0':  # B보다 크고 맨 앞자리 수가 0이 아니면
+            lst.append(int(n))  # 이를 리스트에 추가한다
             return
         else:
             return
     for i in range(len(A)):
         if used[i] == 0:
-            used[i] = 1
-            solve(n+A[i])
+            used[i] = 1 # 사용한 숫자를 재사용하지 않기위해 구분한다.
+            solve(n+A[i])   # 재귀를 돌린다.
             used[i] = 0
 
 A,B = map(int,input().split())
@@ -16,7 +16,7 @@ A = str(A)
 used = [0]*len(A)
 lst = []
 solve('')
-if lst:
-    print(max(lst))
-else:
+if lst: 
+    print(max(lst)) # 리스트의 숫자중 가장 큰 숫자를 출력한다.
+else:   # 리스트가 비었을 경우에는 -1을 출력한다.
     print(-1)
