b938bb8bc579785e4d7460923eed2b0dc7eb709c
Hyungjin Kim <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2024-02-01 11:02:32 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,14 +1,12 @@
 import sys
-a,b,c = map(int,sys.stdin.readline().split())
-
-def multi (a,n):
-  if n == 1:
-      return a%c
-  else:
-      tmp = multi(a,n//2)
-      if n %2 ==0:
-          return (tmp * tmp) % c
-      else:
-          return (tmp  * tmp *a) %c
-          
-print(multi(a,b))
+input = sys.stdin.readline
+def cal(n):
+    if n == 1:
+        return A % C
+    tmp = cal(n//2)
+    if n % 2:
+        return tmp*tmp*A%C
+    else:
+        return tmp*tmp%C
+A,B,C = map(int,input().split())
+print(cal(B))
