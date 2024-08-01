df04b00615801361af02c29448258bb154a59496
Hyungjin Kim <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2024-08-01 14:17:33 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        int n = num_list.length;
+        for(int i=0; i<n; i++){
+            answer[i] = num_list[i];
+        }
+        if(num_list[n-1] > num_list[n-2]){
+            answer[n] = num_list[n-1] - num_list[n-2];
+        }else{
+            answer[n] = num_list[n-1]*2;
+        }
+        return answer;
+    }
+}
